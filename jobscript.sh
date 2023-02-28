9b6f622322709e2d7ec9350b7eacf47ac280527a
Spyros Vlachospyros <spiros.vlachospiros@gmail.com>
2023-02-28 13:59:49 +0100

    fix

--- jobscript.sh
+++ jobscript.sh
@@ -11,7 +11,7 @@
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
 #BSUB -W 24:00
 # request 5GB of system-memory
-#BSUB -R "rusage[mem=5GB]"
+#BSUB -R "rusage[mem=15GB]"
 ### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
