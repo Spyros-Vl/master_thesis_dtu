568e2b2d295b2bdc340d48a26776a58c0f6a49d6
Spyros Vlachospyros <spiros.vlachospiros@gmail.com>
2023-02-28 13:58:46 +0100

    fix

--- src/models/utils.py
+++ src/models/utils.py
@@ -22,7 +22,7 @@ import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator, RPNHead, RegionProposalNetwork
-from torchvision.models import FasterRCNN_ResNet50_FPN_Weights
+from torchvision.models.detection.faster_rcnn import *
 import torch
 
 
