abc5c654751ad63d3ded37ba2b0adeecb88ce027
Spyros Vlachospyros <spiros.vlachospiros@gmail.com>
2023-04-12 11:43:00 +0200

    DETR evaluation function

--- src/visualization/test_model_DETR.py
+++ src/visualization/test_model_DETR.py
@@ -102,14 +102,72 @@ def main():
 
     print("---Per-Class evaluation---")
 
-    coco_gt=train_dataset.coco
-    for catId in coco_gt.getCatIds():
-        print("The metrics for the ",catId)
-        coco_eval = evaluator#COCOeval(coco_gt, coco_dt, "bbox")
-        coco_eval.coco_eval['bbox'].params.catIds = [catId]
-        coco_eval.coco_eval['bbox'].evaluate()
-        coco_eval.coco_eval['bbox'].accumulate()
-        coco_eval.coco_eval['bbox'].summarize()
+    print("---Results for Text")
+    
+    
+
+    evaluator = CocoEvaluator(coco_gt=train_dataset.coco, iou_types=["bbox"])
+    evaluator.coco_eval['bbox'].params.catIds = [0]
+
+    print("Running evaluation...")
+    for idx, batch in enumerate(tqdm(train_dataloader)):
+        # get the inputs
+        pixel_values = batch["pixel_values"].to(device)
+        pixel_mask = batch["pixel_mask"].to(device)
+        labels = [{k: v.to(device) for k, v in t.items()} for t in batch["labels"]] # these are in DETR format, resized + normalized
+
+        # forward pass
+        with torch.no_grad():
+            outputs = model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
+        # turn into a list of dictionaries (one item for each example in the batch)
+        orig_target_sizes = torch.stack([target["orig_size"] for target in labels], dim=0)
+        results = processor.post_process_object_detection(outputs, target_sizes=orig_target_sizes)
+        # provide to metric
+        # metric expects a list of dictionaries, each item 
+        # containing image_id, category_id, bbox and score keys 
+        predictions = {target['image_id'].item(): output for target, output in zip(labels, results)}
+        predictions = prepare_for_coco_detection(predictions)
+        evaluator.update(predictions)
+        
+
+    evaluator.synchronize_between_processes()
+    evaluator.accumulate()
+    evaluator.summarize()
+
+    print("---Results for Fracture")
+    
+    
+
+    evaluator = CocoEvaluator(coco_gt=train_dataset.coco, iou_types=["bbox"])
+    evaluator.coco_eval['bbox'].params.catIds = [1]
+
+    print("Running evaluation...")
+    for idx, batch in enumerate(tqdm(train_dataloader)):
+        # get the inputs
+        pixel_values = batch["pixel_values"].to(device)
+        pixel_mask = batch["pixel_mask"].to(device)
+        labels = [{k: v.to(device) for k, v in t.items()} for t in batch["labels"]] # these are in DETR format, resized + normalized
+
+        # forward pass
+        with torch.no_grad():
+            outputs = model(pixel_values=pixel_values, pixel_mask=pixel_mask)
+
+        # turn into a list of dictionaries (one item for each example in the batch)
+        orig_target_sizes = torch.stack([target["orig_size"] for target in labels], dim=0)
+        results = processor.post_process_object_detection(outputs, target_sizes=orig_target_sizes)
+        # provide to metric
+        # metric expects a list of dictionaries, each item 
+        # containing image_id, category_id, bbox and score keys 
+        predictions = {target['image_id'].item(): output for target, output in zip(labels, results)}
+        predictions = prepare_for_coco_detection(predictions)
+        evaluator.update(predictions)
+        
+
+    evaluator.synchronize_between_processes()
+    evaluator.accumulate()
+    evaluator.summarize()
+
 
 
     print('----------------------Model evaluation ended--------------------------')
